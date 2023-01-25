188dc3a2e6818346977d5147570a2e1e6e876a61
Alessio Limina <alessio.limina@gmail.com>
2023-01-25 16:41:55 +0100

    Exercise: String handling 1

--- src/it/develhope/stringhandling/Start.java
+++ src/it/develhope/stringhandling/Start.java
@@ -3,21 +3,22 @@ package it.develhope.stringhandling;
 public class Start {
     public static void main(String[] args) {
 
-        String string1 = "Hello";
+        System.out.println("---------Starting--------------");
 
+        String string1 = "Hello";
         String string2 = "How are you?";
+
+        System.out.println(string1.indexOf("He"));
+        System.out.println(string2.indexOf("u?"));
 
         char[] charArray = new char[4];
 
-        System.out.println(string1.indexOf("He"));
-
-        System.out.println(string2.indexOf("u?"));
-
-        string1.getChars(0,2,charArray, 0);
-
+        string1.getChars(0,2, charArray, 0);
         string2.getChars(10,12, charArray, 2);
 
         System.out.println(charArray);
+
+        System.out.println("--------------------------------");
 
         }
 
